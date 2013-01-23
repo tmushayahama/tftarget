0fb398068c4b3ad17173c835f455d7b233fea981
Joel Friedly <joelfriedly@gmail.com>
2013-01-23 18:24:39 -0500

    Abstracted AJAX, made enter key submit the form

--- static/js/search.js
+++ static/js/search.js
@@ -7,18 +7,26 @@ function printObject (object) {
 }
 
 
+function ajaxSearch () {
+    $.post('/', $('#search_form').serialize(), function (data) {
+        $('#search_results').children().remove()
+        var i;
+        for (i = 0; i < data.length; i++) {
+            $('#search_results').append("<p>" + printObject(data[i]));
+        }
+    }, 'json');
+}
+
+
 $(document).ready(function () {
     console.log("We're loading jQuery, jQuery UI, and our own custom js!")
 
     $.ajaxSetup({traditional: true});
 
-    $('#search_button').click(function () {
-        $.post('/', $('#search_form').serialize(), function (data) {
-            $('#search_results').children().remove()
-            var i;
-            for (i=0; i < data.length; i++) {
-                $('#search_results').append("<p>" + printObject(data[i]));
-            }
-        }, 'json');
+    $('#search_button').click(ajaxSearch);
+    $('.ctrlHolder').keypress(function (e) {
+        if (e.which == 13) {
+            ajaxSearch();
+        }
     });
 });
