6e3193440b8db91875d1da54f39a3427c9eff628
Joel Friedly <joelfriedly@gmail.com>
2013-01-25 22:20:46 +0000

    Changed form entry order and name

--- search/forms.py
+++ search/forms.py
@@ -3,8 +3,8 @@ from django import forms
 
 class SearchForm(forms.Form):
     """Search form to be submitted by a user."""
+    transcription_family = forms.CharField(label="Transcription Factor Family", required=False)
     transcription_factor = forms.CharField(label="Transcription Factor", required=False)
-    transcription_family = forms.CharField(label="Transcription Family", required=False)
     gene = forms.CharField(label="Gene", required=False)
     species = forms.CharField(label="Species", required=False)
     expt_name = forms.CharField(label="Experiment Name", required=False)
