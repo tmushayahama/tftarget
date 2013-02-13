f47a55f491eabbe79db39abf402ff607d8f87055
Joel Friedly <joelfriedly@gmail.com>
2013-02-13 20:39:37 +0000

    Merged in Tremayne's newest search code, reverted to old layout

--- static/js/search.js
+++ static/js/search.js
@@ -5,6 +5,7 @@
    @date 2/7/2013
 */
 
+
 /**
    This table orders the column of the table. The purpose of the multidimensional array is to 
    map from DB name to Human readable format
@@ -18,12 +19,15 @@ var TABLE_HEADING = [["transcription_factor", "Transcription Factor"],
                      ["cell_line", "Cell Line"],
                      ["expt_type", "Experimental Type"]];
 
+
 var INPUT_NAME = [["id_transcription_family", "Transcription Family"],
                   ["id_transcription_factor", "Transcription Factor"],
                   ["id_gene", "Gene"],
-                  ["id_species", "Experimental Species"],
-                  ["id_tissue_name", "Experimental Tissues"],
-                  ["id_expt_type", "Experimental Type"]];
+                  ["id_species", "Experiment Species"],
+                  ["id_tissue_name", "Experiment Tissues"],
+                  ["id_expt_type", "Experiment Type"]];
+
+
 /**
    Prints the headings of the table from a json object. The result is appended to the table
    @param thead - the thead element of the table
@@ -38,6 +42,8 @@ function printTHead (thead) {
     row += '</tr>'
     thead.append(row);
 }
+
+
 /**
    Prints the rest of the table from a json object. The result is appended to the table
    @param tbody - the tbody element of the table
@@ -57,22 +63,22 @@ function printTBody (tbody, object) {
     tbody.append(row);
 }
 
-/*initSearchForm require that values are in pairs label and a bunch of ctrols.
-*/
+
+//initSearchForm require that values are in pairs label and a bunch of ctrols.
 function initSearchForm () {
+    console.log('Beautifying your search form');
     var $searchForm = $('#tft-search-form').children();
-    /*add the class control-label to all labels in the 
-      input form
-    */
+    /*add the class control-label to all labels in the
+      input form */
     $('label').addClass('control-label');
-    $('.input').wrap('<div class="controls"/>');//every input has a paret of div controls
+    //every input has a parent of div controls
     for(var i=1, j=$searchForm.length; i<j; i+=2) {
-       $searchForm.slice(i, i+2).wrapAll('<div class="control-group"/div>')
+       $searchForm.slice(i, i+2).wrapAll('<div class="control-group"/>')
     }
-    //$searchForm.children(0).remove();
-    //
-    console.log('Beuatifying your search Form');
+    $('.input').wrap('<div class="controls"/>');
 }
+
+
 function searchPreview() {
     //refresh the description
     var $preview = $('#tft-search-preview');
@@ -90,8 +96,11 @@ function searchPreview() {
     }
     $preview.append($dl);
 }
+
+
 function ajaxSearch () {
     $.post('/', $('#tft-search-form').serialize(), function (data) {
+        console.log("Called ajaxSearch");
         //clear the search result for ready for next search result
         $('#search-results').children().remove()
         //create a table here
@@ -110,10 +119,18 @@ function ajaxSearch () {
         }
     }, 'json');
 }
+
+
 $(document).ready(function () {
     console.log("Loading jQuery, jQuery UI, and our own custom js!!!");
     $.ajaxSetup({traditional: true});
-    $('#tft-search-btn-2').click(ajaxSearch);
+    initSearchForm ();
+    addEventHandlers();
+});
+
+
+function addEventHandlers() {
+    $('#tft-search-btn').click(ajaxSearch);
     $('.input-text').keypress(function (e) {
         if (e.which == 13) {
             console.log('enter pressed');
@@ -121,72 +138,4 @@ $(document).ready(function () {
         }
     });
     $('.input-select').change(ajaxSearch);
-    initSearchForm ();
-    addEventHandlers();
-});
-function addEventHandlers() {
-    $('#tft-search-btn-2').click(function (){
-        $('#tft-dialog-form').modal('hide');
-        searchPreview();
-    });
-   // $('#tft-dialog-form').modal();
-    /*$('#tft-dialog-form').dialog({
-        autoOpen: false,
-        height: 600,
-        width: 600,
-        resizable:false,
-        draggable:false,
-        position: "center",
-        modal:true,
-        zIndex:10
-    });
-    $('#tft-advanced-search')
-        .click(function() {
-            $('#tft-dialog-form').dialog('open');
-        });*/
 }
- $(function() {
-
-     var tf = [
-         "E2F",
-         "MYC",
-         "STAT"
-     ];
-     function split( val ) {
-         return val.split( /,\s*/ );
-     }
-     function extractLast( term ) {
-         return split( term ).pop();
-     }
-     $( "#tft-search-query" )
-     // don't navigate away from the field on tab when selecting an item
-         .bind( "keydown", function( event ) {
-             if ( event.keyCode === $.ui.keyCode.TAB &&
-                  $( this ).data( "autocomplete" ).menu.active ) {
-                 event.preventDefault();
-             }
-         })
-         .autocomplete({
-             minLength: 0,
-             source: function( request, response ) {
-                 // delegate back to autocomplete, but extract the last term
-                 response( $.ui.autocomplete.filter(
-                     tf, extractLast( request.term ) ) );
-             },
-             focus: function() {
-                 // prevent value inserted on focus
-                 return false;
-             },
-             select: function( event, ui ) {
-                 var terms = split( this.value );
-                 // remove the current input
-                 terms.pop();
-                 // add the selected item
-                 terms.push( ui.item.value );
-                 // add placeholder to get the comma-and-space at the end
-                 terms.push( "" );
-                 this.value = terms.join( ", " );
-                 return false;
-             }
-         });
- });
